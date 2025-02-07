e736d2be7096631478880348b5b76ccdbb5d8aec
HSKhan-dev <hassankhn2328@gmail.com>
2025-02-08 02:11:43 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,36 @@
+
+let card1img = "file:///C:/Users/A0014/Desktop/Muhammad%20Hassan%20Khan%203813/section-4(1).png";
+let card1 =  "This is my first heading";
+let card1p = "this is the paragraph of whole card";
+let card1button = "Read More";
+let numofcards = 4;
+let myname = "Muhammad Hassan Khan";
+let info = myname;
+let a = 60;
+let b = 10;
+let c = 2;
+let totalvalue = 100
+let multi = a * b;
+let div = b / c;
+let sum = c + a;
+let sub = a - a;
+let perc = a / totalvalue * 100
+
+if(a > b){
+    value = "This is true";
+}
+else {
+    value = "This is false";
+}
+
+console.log(card1img);
+console.log(card1);
+console.log(card1p);
+console.log(card1button);
+console.log(numofcards);
+console.log(info);
+console.log("This is multiplication " + multi);
+console.log("This is divison " + div);
+console.log("This is sum " + sum);
+console.log("This is subtraction " + sub);
+console.log(perc);
